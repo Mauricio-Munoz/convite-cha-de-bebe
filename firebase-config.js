48fe29b695711faa5d9f2ca1705a2bc2bec23913
Mauricio Muñoz <73701014+Mauricio-Munoz@users.noreply.github.com>
2025-04-14 10:39:05 -0300

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+// --- Configuração do Firebase 
+const firebaseConfig = {
+      apiKey: "AIzaSyB6NMSbRKIOyarr2c6oOG7CsXJzuuGQrHw", 
+      authDomain: "chababy001-32f3a.firebaseapp.com", 
+      projectId: "chababy001-32f3a",
+      storageBucket: "chababy001-32f3a.appspot.com", 
+      messagingSenderId: "366959604506", 
+      appId: "1:366959604506:web:0e46294e2c0f8da2bbb2a3"
+};
+
+export default firebaseConfig;
